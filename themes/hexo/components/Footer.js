3e05939ddbc21677e0ea51895b2cd461944e0d32
tangly1024.com <tlyong1992@hotmail.com>
2023-11-01 18:40:00 +0800

    部分配置siteConfig化

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import BLOG from '@/blog.config'
-import DarkModeButton from '@/components/DarkModeButton'
+import { siteConfig } from '@/lib/config'
 
 const Footer = ({ title }) => {
   const d = new Date()
@@ -14,11 +14,11 @@ const Footer = ({ title }) => {
 
   return (
     <footer
-      className='relative dark:bg-black flex-shrink-0 bg-hexo-light-gray justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm p-6 z-10'
+      className='relative z-10 dark:bg-black flex-shrink-0 bg-hexo-light-gray justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm p-6'
     >
-      <DarkModeButton/>
+      {/* <DarkModeButton/> */}
 
-      <i className='fas fa-copyright' /> {`${copyrightDate}`} <span><i className='mx-1 animate-pulse fas fa-heart'/> <a href={BLOG.LINK} className='underline font-bold  dark:text-gray-300 '>{BLOG.AUTHOR}</a>.<br/>
+      <i className='fas fa-copyright' /> {`${copyrightDate}`} <span><i className='mx-1 animate-pulse fas fa-heart'/> <a href={BLOG.LINK} className='underline font-bold  dark:text-gray-300 '>{siteConfig('AUTHOR')}</a>.<br/>
 
       {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{BLOG.BEI_AN}</a><br/></>}
 
