8cb4a92ccd757ff46942860c9104ed69699a4b31
tangly1024.com <tlyong1992@hotmail.com>
2024-01-30 18:05:37 +0800

    theme 优化

--- themes/nav/components/MenuItem.js
+++ themes/nav/components/MenuItem.js
@@ -20,9 +20,6 @@ export const MenuItem = ({ link }) => {
     return null
   }
 
-  // #号加标题  快速跳转到指定锚点
-  const url = link?.to && link?.to.length > 0 ?  link.to : '#' + link?.title
-
   return <>
         {/* 菜单 */}
         <div
@@ -39,7 +36,7 @@ export const MenuItem = ({ link }) => {
                     </div>
                 </>)
               : (
-                    <Link href={url} className='dark:text-neutral-400 dark:hover:text-white font-bold w-full display-block'>
+                    <Link href={link?.to} className='dark:text-neutral-400 dark:hover:text-white font-bold w-full display-block'>
                         {link?.icon && <i className={`text-base ${link?.icon}`} />}{link?.title}
                     </Link>
                 )
@@ -53,9 +50,9 @@ export const MenuItem = ({ link }) => {
                 {
                     link?.subMenus?.map((sLink, index) => (
                         <div key={index} className='nav-submenu'>
-                            <a href={'#' + sLink?.title}>
+                            <Link href={sLink?.to}>
                                 <span className='dark:text-neutral-400 text-gray-500 hover:text-black dark:hover:text-white text-xs font-bold'><i className={`text-xs mr-1 ${sLink?.icon ? sLink?.icon : 'fas fa-hashtag'}`} />{sLink.title}</span>
-                            </a>
+                            </Link>
                         </div>
                     ))
                 }
